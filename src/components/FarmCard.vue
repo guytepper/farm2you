80e35e1e667fdeec303368cc67f90bc04d7a4a8d
Guy Tepper <guytepper@gmail.com>
2016-11-14 15:36:56 +0200

    Farm card init

---
+++ src/components/FarmCard.vue
@@ -0,0 +1,15 @@
+<template >
+  <div class="farm-card">
+    {{ farm.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'farm-card',
+  props: ['farm']
+}
+</script>
+
+<style lang="css">
+</style>
