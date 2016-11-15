8b58e9ffc945c863837d6e70207cb9b50066d8b6
Guy Tepper <guytepper@gmail.com>
2016-11-15 14:10:24 +0200

    Added vue router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
 
-/* eslint-disable no-new */
+Vue.use(VueRouter)
+
+const routes = []
+
+const router = new VueRouter({ routes })
+
 new Vue({
   el: '#app',
   template: '<App/>',
-  components: { App }
+  components: { App },
+  router
 })
