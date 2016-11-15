ab508066ca4c9351dec6c3f771b8b2d490ce8c5a
Guy Tepper <guytepper@gmail.com>
2016-11-15 13:08:50 +0200

    Added farm card (#2)
    * Farm card init
    
    * Imported farm card component
    
    * Load farms from database
    
    * Farm card stylings

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,32 @@
     <img src="./assets/logo.png">
 
     <my-map></my-map>
+
+    <div class="farm-cards">
+      <farm-card v-for="farm in farms" :farm="farm"></farm-card>
+    </div>
   </div>
 </template>
 
 <script>
 import MyMap from './components/Map'
+import FarmCard from './components/FarmCard'
 import axios from 'axios';
 
 export default {
   name: 'app',
   components: {
-    MyMap
+    MyMap, FarmCard
+  },
+  data: function() {
+    return {
+      farms: []
+    }
   },
   created: function() {
+    // Get farms from DB
     axios.get('/api/farm').then(res => {
-      console.log(res);
+      this.$data.farms = res.data;
     });
   }
 }
@@ -28,7 +39,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/FarmCard.vue
@@ -0,0 +1,39 @@
+<template >
+  <div class="farm-card" :farm-id="farm._id">
+    <img src="/static/images/farm-img-placeholder.jpg" alt="" class="farm-card__img"
+        srcset="/static/images/farm-img-placeholder@2x.jpg 2x" />
+    <div class="farm-card__info">
+      <h2 class="farm-card__name">{{ farm.properties.name }}</h2>
+      <div class="farm-card__props">
+        אורגני, משלוחים
+      </div>
+      <div class="farm-card__location">
+        16 ק״מ ממיקומך
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'farm-card',
+  props: ['farm']
+}
+</script>
+
+<style lang="css">
+  .farm-card {
+    display: flex;
+    margin-bottom: 10px;
+  }
+
+  .farm-card__name {
+    margin: 0;
+  }
+
+  .farm-card__img {
+    width: 80px;
+    height: 80px;
+    margin-left: 10px;
+  }
+</style>
