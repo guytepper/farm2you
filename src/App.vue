686ff2a2d3fa9666551104dc0106371f194cea31
Guy Tepper <guytepper@gmail.com>
2016-11-15 15:24:35 +0200

    Removed farm list from App component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-      <my-map></my-map>
 
-      <div class="farm-cards">
-        <farm-list :farms="farms"></farm-list>
-      </div>
+    <my-map></my-map>
 
-    <router-view>
-    </router-view>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -20,20 +15,8 @@ import axios from 'axios';
 export default {
   name: 'app',
   components: {
-    MyMap, FarmList
+    MyMap
   },
-  data: function() {
-    return {
-      farms: []
-    }
-  },
-  created: function() {
-    // Get farms from DB
-    axios.get('/api/farm').then(res => {
-      this.$data.farms = res.data;
-      console.log(this.$data.farms);
-    });
-  }
 }
 </script>
 
