f586785ee8bae0788614a1a5b6c2329a71966719
Guy Tepper <guytepper@gmail.com>
2016-11-15 15:24:13 +0200

    Added initial route

--- src/main.js
+++ src/main.js
@@ -4,7 +4,13 @@ import App from './App'
 
 Vue.use(VueRouter)
 
-const routes = []
+const Foo = { template: '<div>foo</div>' }
+import FarmList from './components/FarmList';
+
+const routes = [
+  { path: '/', component: FarmList },
+  { path: '/foo', component: Foo },
+]
 
 const router = new VueRouter({ routes })
 
