8f86b39ea1b87ce13c1fadfd4535f89eba895abd
guytepper <guytepper@gmail.com>
2016-11-17 17:09:37 +0200

    Added app header styles

--- src/App.vue
+++ src/App.vue
@@ -20,13 +20,17 @@ export default {
 }
 </script>
 
-<style>
-@import url('./assets/style.css');
+<style lang="scss">
+body {
+  margin: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+}
+
+.container {
+    padding: 0 20px;
 }
 </style>
--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1>ישר מהשדה</h1>
+  <header class="header container">
+    <h1 class="site-title">ישר מהשדה</h1>
+  </header>
 </template>
 
 <script>
@@ -8,5 +10,17 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style lang="scss">
+  @import '../../assets/scss/theme.scss';
+
+  .header {
+    width: 100%;
+    height: 45px;
+    background-color: $primary-color;
+  }
+
+  .site-title {
+    margin: 0;
+    color: $primary-color-text;
+  }
 </style>
