2dbb486c0f15c176620bba423f821e748bee6bfc
Guy Tepper <guytepper@gmail.com>
2016-11-18 18:49:48 +0200

    Get farm using route ID

--- src/components/FarmPage.vue
+++ src/components/FarmPage.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
-    {{ farm }}
+    {{ farm.properties.name }}
   </div>
 </template>
 
 <script>
 export default {
   name: 'farm-page',
-  props: ['farm']
+  data () {
+    return {
+      farm: {}
+    }
+  },
+  created () {
+    // Get farm using ID parameter from route
+    const farmId = parseInt(this.$route.params.id);
+    const farms = this.$store.state.farms;
+    this.farm = farms.find(farm => farm.id === farmId)
+  }
 }
 </script>
 
