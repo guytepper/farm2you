211e86fea6087c21439d01b66b0c55edd24eba29
guytepper <guytepper@gmail.com>
2016-11-27 16:09:24 +0200

    Added mui css lib

--- src/components/farm/forms/AddFarm.vue
+++ src/components/farm/forms/AddFarm.vue
@@ -1,6 +1,28 @@
 <template>
-  <div class="">
-    <gm-autocomplete v-model="place"></gm-autocomplete>
+  <div class="add-farm">
+
+
+    <div class="mui-textfield">
+      <input type="text" id="farm-name" placeholder="שם המשק">
+      <label for="farm-name">שם המשק</label>
+    </div>
+
+    <div class="mui-textfield">
+      <gm-autocomplete v-model="place"></gm-autocomplete>
+      <label for="location">מיקום</label>
+    </div>
+
+    <div class="mui-textfield">
+      <input type="tel" id="phone" placeholder="טלפון המשק">
+      <label for="phone">טלפון</label>
+    </div>
+
+    <div class="mui-textfield">
+      <input type="website" id="website" placeholder="טלפון המשק">
+      <label for="website">טלפון</label>
+    </div>
+
+
   </div>
 </template>
 
@@ -12,11 +34,19 @@ export default {
   components: { 'gm-autocomplete': GMAutocomplete },
   data () {
     return {
-      place: null
+      name: '',
+      location: null
     }
   }
 }
 </script>
 
-<style>
+<style style="scss">
+@import '~muicss/dist/css/mui-rtl.css';
+
+.add-farm {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
 </style>
