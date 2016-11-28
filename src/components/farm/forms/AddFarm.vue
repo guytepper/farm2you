0cbda70f89e3de945c69cd304a5c4e68997f357c
Guy Tepper <guytepper@gmail.com>
2016-11-28 15:44:02 +0200

    Added heAttributes to VeeValidate dict

--- src/components/farm/forms/AddFarm.vue
+++ src/components/farm/forms/AddFarm.vue
@@ -68,13 +68,15 @@
 import Vue from 'vue';
 import VeeValidate from 'vee-validate';
 import GMAutocomplete from './helpers/GMAutocomplete';
-import heMessages from './helpers/heMessages.js';
+import heMessages from './strings/heMessages.js';
+import heAttributes from './strings/heAttributes.js';
 
 Vue.use(VeeValidate, {
   locale: 'he',
   dictionary: {
     he: {
         messages: heMessages
+        attributes: heAttributes
     }
   }
 });
