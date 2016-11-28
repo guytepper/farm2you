ea7fe398208a1bbbfc43c0a575efb9393a2f4f3b
Guy Tepper <guytepper@gmail.com>
2016-11-28 17:09:48 +0200

    Added attributes to dict

--- src/components/farm/forms/strings/heAttributes.js
+++ src/components/farm/forms/strings/heAttributes.js
@@ -1,5 +1,10 @@
-// Hebrew messages for the vee-validate plugin
+// Hebrew attributes for the vee-validate plugin
 
 export default {
-    'farm-name': 'שם המשק'
+    'farm-name': 'שם המשק',
+    'email': 'מיקום',
+    'phone': 'טלפון המשק',
+    'email': 'אימייל',
+    'website': 'אתר אינטרנט',
+    'facebook': 'פייסבוק'
 };
