fc5cc14d588ef16f33d48602c012df421c1f22c8
Guy Tepper <guytepper@gmail.com>
2016-11-30 14:08:56 +0200

    Removed FarmForm module

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Vuex from 'Vuex'
 import VuexFire from 'vuexfire'
 import Firebase from 'firebase'
-import FarmForm from './modules/FarmForm'
 
 Vue.use(Vuex)
 Vue.use(VuexFire)
@@ -14,8 +13,5 @@ export default new Vuex.Store({
   mutations: VuexFire.mutations,
   getters: {
     farms: state => state.farms,
-  },
-  modules: {
-    FarmForm
   }
 });
