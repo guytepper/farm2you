78a17d652fa964e4fda60abd34a95854afba2655
Guy Tepper <guytepper@gmail.com>
2016-12-04 12:27:07 +0200

    Moved login page component location

--- src/components/forms/Auth/LoginPage.vue
+++ src/components/forms/Auth/LoginPage.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="panel login-page">
-    <form class="login-form">
+    <form class="login-form" @submit.prevent="signIn">
       <div class="mui-textfield mui-textfield--float-label">
-        <input id="username" name="username" type="text">
+        <input v-model="username" id="username" name="username" type="text">
         <label for="username">שם משתמש</label>
       </div>
 
       <div class="mui-textfield mui-textfield--float-label">
-        <input id="password" name="password" type="password">
+        <input v-model="password" id="password" name="password" type="password">
         <label for="password">סיסמא</label>
       </div>
 
@@ -20,13 +20,25 @@
 </template>
 
 <script>
+import auth from '../helpers/Auth.js'
 export default {
+  data () {
+    return {
+      username: "",
+      password: ""
+    }
+  },
+  methods: {
+    signIn () {
+      auth.signIn(this.username, this.password);
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-@import "../../assets/vendor/mui/textfield";
-@import "../../assets/vendor/mui/button";
+@import "../../../assets/vendor/mui/textfield";
+@import "../../../assets/vendor/mui/button";
 
 .login-form {
   width: 50%;
