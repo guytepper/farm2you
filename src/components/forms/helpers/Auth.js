c6ee6e5dc1513432e6b449f1b7532272042d7c5d
Guy Tepper <guytepper@gmail.com>
2016-12-04 12:25:42 +0200

    Added auth module

---
+++ src/components/forms/helpers/Auth.js
@@ -0,0 +1,13 @@
+import firebase from '../../../config/firebase';
+
+const auth = firebase.auth();
+
+export default {
+  signIn(email, password) {
+    auth.signInWithEmailAndPassword(email, password).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(`${errorCode}: ${errorMessage}`);
+    })
+  },
+}
