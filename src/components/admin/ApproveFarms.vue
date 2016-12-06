b121bce673ce94fec8d4eae3b70d0082504a31cc
Guy Tepper <guytepper@gmail.com>
2016-12-06 17:50:14 +0200

    Added dummy data

--- src/components/admin/ApproveFarms.vue
+++ src/components/admin/ApproveFarms.vue
@@ -2,7 +2,18 @@
   <div class="panel">
     <h1>משק בן יהודה</h1>
     <div class="divider"></div>
-
+    <div class="">
+      <span>כתובת:</span>
+      <span>הרימונים 6, בית יצחק</span>
+    </div>
+    <div class="">
+      <span>טלפון:</span>
+      <span>052-8656710</span>
+    </div>
+    <div class="">
+      <span>אימייל:</span>
+      <span>guytepper@gmail.com</span>
+    </div>
   </div>
 </template>
 
