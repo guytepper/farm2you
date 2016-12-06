b793d931a68578f1b0d46fdbb9b515107a6732a5
Guy Tepper <guytepper@gmail.com>
2016-12-06 19:09:20 +0200

    Added props

--- src/components/admin/ApproveFarms.vue
+++ src/components/admin/ApproveFarms.vue
@@ -1,20 +1,56 @@
 <template>
-  <div class="pending_farms">
-    <div v-for="farm in farms" class="panel">
+  <div class="pending-farms">
+    <div v-for="farm in farms" class="panel pending-farm">
       <h1>{{ farm.name }}</h1>
       <div class="divider"></div>
-      <div class="">
-        <span>כתובת:</span>
+      <div class="pending_farm__info">
+        <span class="pending-farm__info-label">כתובת:</span>
         <span>{{ farm.address }}</span>
       </div>
-      <div class="">
-        <span>טלפון:</span>
+      <div class="pending_farm__info">
+        <span class="pending-farm__info-label">טלפון:</span>
         <span>{{ farm.phone }}</span>
       </div>
-      <div class="">
-        <span>אימייל:</span>
+      <div class="pending_farm__info">
+        <span class="pending-farm__info-label">אימייל:</span>
         <span>{{ farm.email }}</span>
       </div>
+
+      <div class="mui-checkbox">
+        <label>
+          <input type="checkbox" :checked="farm.props.organic" disabled>
+          אורגני
+        </label>
+      </div>
+
+      <div class="mui-checkbox">
+        <label>
+          <input type="checkbox" :checked="farm.props.direct_sell" disabled>
+          מכירה ישירה
+        </label>
+      </div>
+
+      <div class="mui-checkbox">
+        <label>
+          <input type="checkbox" :checked="farm.props.online_sell" disabled>
+            מכירה באינטרנט
+        </label>
+      </div>
+
+      <div class="mui-checkbox">
+        <label>
+          <input type="checkbox" :checked="farm.props.shipping" disabled>
+          משלוחים
+        </label>
+      </div>
+
+      <div class="mui-checkbox">
+        <label>
+          <input type="checkbox" :checked="farm.props.kosher" disabled>
+          כשר
+        </label>
+      </div>
+
     </div>
   </div>
 </template>
@@ -30,4 +66,9 @@ export default {
 </script>
 
 <style lang="scss">
+
+.pending-farm__info-label {
+  font-weight: bold
+}
+
 </style>
