5c4314d04d43398f85509106e474618afb270058
Guy Tepper <guytepper@gmail.com>
2016-12-06 19:13:44 +0200

    Farm style modifications

--- src/components/admin/ApproveFarms.vue
+++ src/components/admin/ApproveFarms.vue
@@ -1,56 +1,57 @@
 <template>
   <div class="pending-farms">
     <div v-for="farm in farms" class="panel pending-farm">
-      <h1>{{ farm.name }}</h1>
+      <h1 class="pending-farm__name">{{ farm.name }}</h1>
       <div class="divider"></div>
-      <div class="pending_farm__info">
-        <span class="pending-farm__info-label">כתובת:</span>
-        <span>{{ farm.address }}</span>
-      </div>
-      <div class="pending_farm__info">
-        <span class="pending-farm__info-label">טלפון:</span>
-        <span>{{ farm.phone }}</span>
-      </div>
-      <div class="pending_farm__info">
-        <span class="pending-farm__info-label">אימייל:</span>
-        <span>{{ farm.email }}</span>
-      </div>
+      <div class="pending-farm__info">
+        <div>
+          <span class="pending-farm__info-label">כתובת:</span>
+          <span>{{ farm.address }}</span>
+        </div>
+        <div>
+          <span class="pending-farm__info-label">טלפון:</span>
+          <span>{{ farm.phone }}</span>
+        </div>
+        <div>
+          <span class="pending-farm__info-label">אימייל:</span>
+          <span>{{ farm.email }}</span>
+        </div>
 
-      <div class="mui-checkbox">
-        <label>
-          <input type="checkbox" :checked="farm.props.organic" disabled>
-          אורגני
-        </label>
-      </div>
+        <div class="mui-checkbox">
+          <label>
+            <input type="checkbox" :checked="farm.props.organic" disabled>
+            אורגני
+          </label>
+        </div>
 
-      <div class="mui-checkbox">
-        <label>
-          <input type="checkbox" :checked="farm.props.direct_sell" disabled>
-          מכירה ישירה
-        </label>
-      </div>
+        <div class="mui-checkbox">
+          <label>
+            <input type="checkbox" :checked="farm.props.direct_sell" disabled>
+            מכירה ישירה
+          </label>
+        </div>
 
-      <div class="mui-checkbox">
-        <label>
-          <input type="checkbox" :checked="farm.props.online_sell" disabled>
-            מכירה באינטרנט
-        </label>
-      </div>
+        <div class="mui-checkbox">
+          <label>
+            <input type="checkbox" :checked="farm.props.online_sell" disabled>
+              מכירה באינטרנט
+          </label>
+        </div>
 
-      <div class="mui-checkbox">
-        <label>
-          <input type="checkbox" :checked="farm.props.shipping" disabled>
-          משלוחים
-        </label>
-      </div>
+        <div class="mui-checkbox">
+          <label>
+            <input type="checkbox" :checked="farm.props.shipping" disabled>
+            משלוחים
+          </label>
+        </div>
 
-      <div class="mui-checkbox">
-        <label>
-          <input type="checkbox" :checked="farm.props.kosher" disabled>
-          כשר
-        </label>
+        <div class="mui-checkbox">
+          <label>
+            <input type="checkbox" :checked="farm.props.kosher" disabled>
+            כשר
+          </label>
+        </div>
       </div>
-
     </div>
   </div>
 </template>
@@ -66,6 +67,14 @@ export default {
 </script>
 
 <style lang="scss">
+
+.pending-farm__name {
+  margin: 0 0 10px;
+}
+
+.pending-farm__info {
+  margin-top: 10px;
+}
 
 .pending-farm__info-label {
   font-weight: bold
