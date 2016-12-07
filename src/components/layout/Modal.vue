b7aebb881c49b52a551c0fd5bcde8d0b189436a0
guytepper <guytepper@gmail.com>
2016-12-07 19:49:38 +0200

    Prevent submission on modal button click

--- src/components/layout/Modal.vue
+++ src/components/layout/Modal.vue
@@ -19,7 +19,7 @@
           <div class="modal-footer">
             <slot name="footer">
 
-              <button class="modal-default-button" @click="$emit('close')">
+              <button class="modal-default-button" @click.prevent="$emit('close')">
                 אוקיי!
               </button>
             </slot>
@@ -63,7 +63,6 @@ export default {
   border-radius: 2px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
   transition: all .3s ease;
-  font-family: Helvetica, Arial, sans-serif;
 }
 
 .modal-header h3 {
