0f9814f06130fd9a16e78904f1cdfb85548b5fef
Guy Tepper <guytepper@gmail.com>
2016-12-13 10:45:50 +0200

    Removed signIn method [TEMP]

--- src/helpers/Auth.js
+++ src/helpers/Auth.js
@@ -4,13 +4,6 @@ import firebaseLib from 'firebase';
 const auth = firebase.auth();
 
 export default {
-  signIn(email, password) {
-    auth.signInWithEmailAndPassword(email, password).catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(`${errorCode}: ${errorMessage}`);
-    })
-  },
   signOut() {
     auth.signOut();
   },
