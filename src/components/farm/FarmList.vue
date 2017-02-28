7bb599370e9d98524b5c35706098cda2cba3f7c2
Guy Tepper <guytepper@gmail.com>
2017-02-28 18:37:26 +0200

    Display farms around user location (#32)
    Currently using static location & radius

--- src/components/farm/FarmList.vue
+++ src/components/farm/FarmList.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+import GeoFire from 'geofire';
 import FarmCard from './FarmCard'
 
 export default {
@@ -14,8 +15,26 @@ export default {
       farms: []
     }
   },
+  methods: {
+    // Finds the closest farms to the user location
+    getClosestFarms () {
+      const geoFire = new GeoFire(this.$root.$firebaseRefs.locations);
+      const geoQuery = geoFire.query({
+        center: [32.290315299999996, 34.9400146],
+        radius: 10.5
+      });
+
+      // Add the farms that meeting the query's criterias to the farms list
+      geoQuery.on("key_entered", (key, location, distance) => {
+        // Retrieve the farm from the farms list using it's key
+        const farm = this.$store.state.farms.find(farm => farm['.key'] === key);
+        this.farms.push(farm)
+      });
+
+    }
+  },
   created () {
-    this.farms = this.$store.state.farms
+    this.getClosestFarms();
   }
 }
 </script>
