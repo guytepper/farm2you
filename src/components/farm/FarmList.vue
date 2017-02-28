81807fec24872c96a858bfb43b244eb9cf48c6bc
guytepper <guytepper@gmail.com>
2017-02-28 18:31:47 +0200

    Started working on getClosestFarms method

--- src/components/farm/FarmList.vue
+++ src/components/farm/FarmList.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+import GeoFire from 'geofire';
 import FarmCard from './FarmCard'
 
 export default {
@@ -14,7 +15,27 @@ export default {
       farms: []
     }
   },
+  methods: {
+    // Finds the closest farms to the user location
+    getClosestFarms () {
+      const geoFire = new GeoFire(this.$root.$firebaseRefs.locations);
+      const geoQuery = geoFire.query({
+        center: [32.290315299999996, 34.9400146],
+        radius: 10.5
+      });
+
+      // Add the farms that meeting the query's criterias to the farms list
+      geoQuery.on("key_entered", (key, location, distance) => {
+        // Retrieve the farm from the farms list using it's key
+        const farm = this.$store.state.farms.find(key);
+        console.log(farm);
+        // this.farms.push(this.$store.state)
+      });
+
+    }
+  },
   created () {
+    this.getClosestFarms();
     this.farms = this.$store.state.farms
   }
 }
