dbff8953693c3718b36a103c9c7faabe09d04677
guytepper <guytepper@gmail.com>
2017-03-01 21:56:23 +0200

    Removed pending farms router link

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -9,7 +9,6 @@
     <div class="header__left-section">
       <router-link to="/add-farm/" class="header__link">הוספת משק</router-link>
       <div v-if="this.$store.state.User.info">
-        <router-link v-if="this.$store.state.User.isAdmin" to="/admin/pending-farms" class="header__link">אישור משקים</router-link>
         <a href="#"  @click="signOut" class="header__link">התנתקות</a>
       </div>
     </div>
